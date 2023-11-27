86caba290f7b52fe196a8e68b6b34e10c41fc8ee
Dusha_Ada <lola180880mail.ru@gmail.com>
2023-11-27 19:39:49 +0500

    //Lagrange

---
+++ ConsoleApp1/Lagrange.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lagrange
+{
+    class Lagrange
+    {
+        private double l(int index, double[] X, double x)
+        {
+            double l = 1;
+            for (int i = 0; i < X.Length; i++)
+            {
+                if (i != index)
+                {
+                    l *= (x - X[i]) / (X[index] - X[i]);
+                }
+            }
+            return l;
+        }
+
+        public double GetValue(double[] X, double[] Y, double x)
+        {
+            double y = 0;
+            for (int i = 0; i < X.Length; i++)
+            {
+                y += Y[i] * l(i, X, x);
+            }
+
+            return y;
+        }
+
+        private double l(int index, double[] X, double h, double x)
+        {
+            double l = 1;
+            for (int i = 0; i < X.Length; i++)
+            {
+                if (i != index)
+                {
+                    l *= (x - X[0] - i * h) / (index - i);
+                }
+            }
+            return l / Math.Pow(h, X.Length - 1);
+        }
+
+        public double GetValue(double[] X, double h, double[] Y, double x)
+        {
+            double y = 0;
+            for (int i = 0; i < X.Length; i++)
+            {
+                y += Y[i] * l(i, X, h, x);
+            }
+
+            return y;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lagrange
+{
+    class Program
+    {
+        public static double F(double x)
+        {
+            return -2 * x + 6 + x * x;
+        }
+        static void Main()
+        {
+            double[] X = new double[] { 0, 1, 2, 3, 4, 5, 6 };
+            double[] Y = new double[X.Length];
+            double x = 4.5;
+            for(int i = 0; i < X.Length; i++)
+            {
+                Y[i] = F(X[i]);
+            }
+            Lagrange lagrange = new Lagrange();
+            double y = lagrange.GetValue(X,Y,x);
+            Console.WriteLine($"Intepolation by Lagrage for X = {x}, Y = {y}");
+            Console.WriteLine($"True value for X = {x}, Y = {F(x)} ");
+
+
+
+        }
+
+    }
+}
